afa6f0242d1287392fdcd60c63d80681d5fda606
Koungkea101 <nannkoungkea00@gmail.com>
2024-11-20 21:50:03 +0700

    Finish Ex1 and Ex2 of TP3: 	+create store 	+create 4 getters and test them in console log

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <div class="page">
     This is my first VueJs Project
@@ -20,10 +16,15 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <script>
+
+
+  import { mapState } from 'pinia';
   import categoryComp from './components/categoryComp.vue';
   import ButtonComp from './components/buttonComp.vue';
   import PromotionComp from './components/promotionComp.vue';
   import axios from 'axios';
+  import { get } from 'spotify';
+  import { useProductStore } from './stores/product';
 
   export default {
     name: 'App',
@@ -32,35 +33,73 @@ import { RouterLink, RouterView } from 'vue-router'
       ButtonComp,
       PromotionComp
     },
+    setup() {
+      const store = useProductStore();
+
+
+      return { store };
+    },
 
     data(){
       return{
         categories:[
         ],
         promotions:[
-        ]
+        ],
+        currentGroupName: 'Meats',
       }
     },
     //fetch categories and promotions
-    mounted(){
+    async mounted(){
       
-      this.fetchCategories();
-      this.fetchPromotions();
+      await this.fetchCategories();
+      await this.fetchPromotions();
+      await this.store.fetchProducts();
+      await this.store.fetchGroups();
+      await this.store.fetchCategories();
+
+      console.log("product: ",this.store.getProductsByGroup(this.currentGroupName));
+      console.log("All product: ",this.store.products);
+
+      //testing getter
+      console.log("groupName: ",this.store.getCategoriesByGroup("Milks & Diaries"));
+      console.log("Fruits: ",this.store.getProductsByGroup("Fruits"));
+      console.log("categoryId: ",this.store.getProductsByCategory(2));
+      console.log("popular: ", this.popularProducts);
+
+
     },
     methods: {
       fetchCategories(){
         axios.get("http://localhost:3000/api/categories").then((result1) => {
-          console.log(result1.data);
+          // console.log(result1.data);
           this.categories = result1.data;
         })
       },
       fetchPromotions(){
         axios.get("http://localhost:3000/api/promotions").then((result) => {
-          console.log(result.data);
+          // console.log(result.data);
           this.promotions = result.data;
         });
       }
-    }
+    },
+
+    computed: {
+      ...mapState(useProductStore, {
+        popularProducts:'getPopularProducts',
+        categories(store){
+          return this.store.getCategoriesByGroup(this.currentGroupName);
+          
+        },
+        
+        products(store) {
+          console.log()
+          return this.store.getProductsByGroup(this.currentGroupName); 
+        },
+
+      })
+    },
+    
   };
 </script>
 
---
+++ src/stores/product.js
@@ -0,0 +1,56 @@
+import { defineStore, mapState } from 'pinia';
+import axios from 'axios';
+
+export const useProductStore = defineStore('product', {
+  state: () => ({
+    groups: [],
+    products: [],
+    promotions: [],
+    categories: [],
+  }),
+
+  getters: {
+    getCategoriesByGroup: (state) => {
+      return(groupName)=> state.categories.filter(category => category.group === groupName);
+    },
+    getProductsByGroup: (state) => {
+      return (groupName)=>state.products.filter(product => product.group === groupName);
+    },
+
+    getProductsByCategory: (state) => {
+      return (categoryName)=>state.products.filter(product => product.categoryId === categoryName);
+    },
+    getPopularProducts: (state) => {
+      return state.products.filter(product => product.countSold > 10);
+    }
+  },
+
+  actions: {
+    async fetchGroups() {
+      try {
+        const response = await axios.get("http://localhost:3000/api/groups");
+        this.groups = response.data;
+        console.log(this.groups);
+      } catch (error) {
+        console.error("Error fetching groups:", error);
+      }
+    },
+    
+    async fetchProducts() {
+      try {
+        const response = await axios.get("http://localhost:3000/api/products");
+        this.products = response.data;
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    },
+    async fetchCategories() {
+      try {
+        const response = await axios.get("http://localhost:3000/api/categories");
+        this.categories = response.data;
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    },
+  },
+});
