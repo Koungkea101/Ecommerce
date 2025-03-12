7f4eb660087da99bac21b893568bafbe57890b0e
Koungkea101 <nannkoungkea00@gmail.com>
2025-03-12 08:47:32 +0700

    TD2Blog

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -9,23 +9,27 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::controller(CategoryController::class)->prefix('categories')->group(function () {
-    Route::get('/', 'getCategories');
-    Route::post('/', 'createCategory');
-    Route::get('/{categoryId}', 'getCategory');
-    Route::put('/{categoryId}', 'updateCategory');
-    Route::delete('/{categoryId}', 'deleteCategory');
-});
+// Route::controller(CategoryController::class)->prefix('categories')->group(function () {
+//     Route::get('/', 'getCategories');
+//     Route::post('/', 'createCategory');
+//     Route::get('/{categoryId}', 'getCategory');
+//     Route::put('/{categoryId}', 'updateCategory');
+//     Route::delete('/{categoryId}', 'deleteCategory');
+// });
 
-Route::controller(ProductController::class)->prefix('products')->group(function () {
-    Route::get('/', 'getProducts');
-    Route::post('/', 'createProduct');
-    Route::get('/{productId}', 'getProduct');
-    Route::put('/{productId}', 'updateProduct');
-    Route::delete('/{productId}', 'deleteProduct');
-});
+// Route::controller(ProductController::class)->prefix('products')->group(function () {
+//     Route::get('/', 'getProducts');
+//     Route::post('/', 'createProduct');
+//     Route::get('/{productId}', 'getProduct');
+//     Route::put('/{productId}', 'updateProduct');
+//     Route::delete('/{productId}', 'deleteProduct');
+// });
 
-// Route::resource('categories', CategoryController::class)->withoutMiddleware([\App\Http\Middleware\Authenticate::class]);
-// Route::resource('products', ProductController::class)->withoutMiddleware([\App\Http\Middleware\Authenticate::class]);
-Route::get('/products', [ProductController::class, 'getProducts']);
-Route::get('/categories', [CategoryController::class, 'getCategories']);
+// // Route::resource('categories', CategoryController::class)->withoutMiddleware([\App\Http\Middleware\Authenticate::class]);
+// // Route::resource('products', ProductController::class)->withoutMiddleware([\App\Http\Middleware\Authenticate::class]);
+// Route::get('/products', [ProductController::class, 'getProducts']);
+// Route::get('/categories', [CategoryController::class, 'getCategories']);
+Route::get('/articles', [ArticleController::class, 'getArticles']);
+Route::post('/articles', [ArticleController::class, 'createArticle']);
+Route::patch('/articles/{id}', [ArticleController::class, 'updateArticle']);
+Route::delete('/articles/{id}', [ArticleController::class, 'deleteArticle']);
